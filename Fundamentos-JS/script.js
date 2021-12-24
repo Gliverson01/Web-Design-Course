619ae74d77131e742e1461662e2992195db2f00f
Gliverson Almeida <gliverson2014@gmail.com>
2021-12-24 15:29:56 -0300

    Operações Matemáticas

--- Fundamentos-JS/script.js
+++ Fundamentos-JS/script.js
@@ -1,3 +1,16 @@
 let mensagem = "Olá Mundo!";
 
 alert(mensagem);
+
+var a = 1;
+var b = 2;
+
+var soma = a+b;
+var sub = a-b;
+var mult = a*b;
+var div = a/b;
+
+alert(soma);
+alert(sub);
+alert(mult);
+alert(div);
